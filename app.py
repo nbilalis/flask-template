01e797f603ace5d1d1e7ef65480b5e93382466a5
Nicholaos Bilalis <n.bilalis@sae.edu>
2022-07-21 11:49:50 +0300

    Add `Flask-DebugToolbar`

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
 from os import environ
 
 app = Flask(__name__)
+
+# python -c 'import secrets; print(secrets.token_hex())'
+app.secret_key = environ.get('SECRET_KEY', '05a691f2eee3a3b65bf680461babbfa173c6a30d61746622165eb837c2873b24')
+
+toolbar = DebugToolbarExtension(app)
 
 
 @app.route('/')
